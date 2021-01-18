ac53291363785f4e2767fbb2581c0b656b005a65
skspyapa <supun@rumex.lk>
2021-01-18 20:16:32 +0530

    add logging

--- web/src/main/java/lk/rumex/user/management/controller/UserController.java
+++ web/src/main/java/lk/rumex/user/management/controller/UserController.java
@@ -3,6 +3,8 @@ package lk.rumex.user.management.controller;
 import lk.rumex.user.management.dto.CreateUserDTO;
 import lk.rumex.user.management.model.User;
 import lk.rumex.user.management.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,27 +16,50 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
   @RequestMapping(value = "/users", method = RequestMethod.GET)
   public List<User> getAllUsers() {
-    return userService.findAll();
+    logger.info("Get all user method executing started");
+
+    List<User> all = userService.findAll();
+
+    logger.info("Get all user method executing stopped");
+
+    return all;
   }
 
   @RequestMapping(value = "/users", method = RequestMethod.POST)
   public User createUser(@RequestBody CreateUserDTO createUserDTO) {
+    logger.info("Create user method executing started");
 
-    return userService.create(createUserDTO);
+    User user = userService.create(createUserDTO);
+
+    logger.info("Create user method executing stopped");
+
+    return user;
   }
 
   @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
   public Long deleteUser(@PathVariable(name = "id") Long id) {
+    logger.info("Delete user method executing started");
 
-    return userService.delete(id);
+    Long delete = userService.delete(id);
+
+    logger.info("Delete user method executing stopped");
+
+    return delete;
   }
 
   @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
   public User findById(@PathVariable(name = "id") Long id) {
+    logger.info("Get user by id method executing started");
 
-    return userService.findById(id);
+    User byId = userService.findById(id);
+
+    logger.info("Get user by id method executing stopped");
+
+    return byId;
   }
 
 }
--- web/src/main/java/lk/rumex/user/management/service/impl/UserServiceImpl.java
+++ web/src/main/java/lk/rumex/user/management/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package lk.rumex.user.management.service.impl;
 
 import lk.rumex.user.management.Repository.UserRepository;
+import lk.rumex.user.management.controller.UserController;
 import lk.rumex.user.management.dto.CreateUserDTO;
-import lk.rumex.user.management.exception.UserNotFoundException;
 import lk.rumex.user.management.model.User;
 import lk.rumex.user.management.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,8 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserRepository userRepository;
+
+  private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
   @Override
   public List<User> findAll() {
@@ -54,6 +58,7 @@ public class UserServiceImpl implements UserService {
     if (present) {
       return userRepository.findById(id).get();
     } else {
+      logger.error("No user for id [{}]", id);
       return null;
     }
   }
